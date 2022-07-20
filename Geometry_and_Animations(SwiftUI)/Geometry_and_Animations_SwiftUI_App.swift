2959275803fa34a04d3b176607cc39e4f29ea863
Sergey-Yurtaev <engineerk2@mail.ru>
2022-07-20 22:46:55 +0500

    Initial Commit

---
+++ Geometry_and_Animations(SwiftUI)/Geometry_and_Animations_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Geometry_and_Animations_SwiftUI_App.swift
+//  Geometry_and_Animations(SwiftUI)
+//
+//  Created by  Sergey Yurtaev on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Geometry_and_Animations_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
